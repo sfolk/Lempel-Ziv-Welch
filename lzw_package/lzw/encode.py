442db69d1c6199268301b28f6944a799e70cab97
sfolk <sarabyfolk@gmail.com>
2021-03-30 10:04:44 +0200

    unit bit as a base: avoid returning extra_bits

--- lzw_package/lzw/encode.py
+++ lzw_package/lzw/encode.py
@@ -5,33 +5,27 @@ from bitarray import frozenbitarray
 
 def lzw_encode(input_seq):
     #instantiate variables
-    unit = 8 
+    unit = 1 
     e_seq = []
     d = dict( zip( [frozenbitarray(np.binary_repr(x, unit)) for x in range(2**unit)], np.arange(2**unit)))
 
     #check input validity
     if input_seq == None or len(input_seq) == 0:
-        return None, None
+        return None
     elif not set(input_seq).issubset({'0', '1'}) and bool(input_seq):
             print("WARNING: only binary inputs are allowed")
-            return None, None
+            return None
 
-    if len(input_seq) % unit != 0:
-        d_seq = bitarray(input_seq)
-        n_added = d_seq.fill()  
-        print( "WARNING!! In the following result, {} bits were added ".format(n_added))
-        d_seq = frozenbitarray(d_seq)
-    else:
-        n_added = 0
-        d_seq = frozenbitarray(input_seq)
+
+    d_seq = frozenbitarray(input_seq)
 
 
     #begin algorthm
     p = d_seq[:unit]
 
-    for i in range(1, int(len(d_seq)/8)):
+    for i in range(1, int(len(d_seq)/unit)):
         
-        c = d_seq[8*i:8*(i+1)]
+        c = d_seq[unit*i:unit*(i+1)]
         pc = p+c
 
         if d.get( pc ) != None :
@@ -46,5 +40,6 @@ def lzw_encode(input_seq):
 
     e_seq.append( '{0:03}'.format(d.get( p )) )
     #print(  'encoded sequence', ''.join(e_seq))
-    return ''.join(e_seq) , n_added
+    return ''.join(e_seq) 
 
+#print(lzw_encode('1001110'))
